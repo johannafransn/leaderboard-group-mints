36109e4828c2a15c5ce3104593e4313a16478da7
Johanna Fransson <adrina0002@gmail.com>
2023-11-01 18:44:32 -0300

    add more styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,11 @@ const Navbar = () => {
       <nav className=" navbar navbar-expand-lg sticky-top navbar-light bg-white border-bottom shadow-sm ">
         <div className="container" style={{ color: "black" }}>
           <a className="navbar-brand" href="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="44"
-              height="44"
-              fill="#a06ffb"
-              className="bi bi-arrow-repeat"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" />
-              <path
-                fillRule="evenodd"
-                d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
-              />
-            </svg>{" "}
-            Honey Pot Leaderboards
+            <img
+              height="20"
+              src="https://uploads-ssl.webflow.com/63e610c62d73bd54e8ee8455/63e610c62d73bd46ffee8583_Liquality_logo.svg"
+              alt="nav"
+            />{" "}
           </a>
           <div className="navbar-collapse collapse show" id="navbarNav">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,25 @@ import { useState } from "react";
 export default function Home({ propsHere }) {
   const [interval, setInterval] = useState(10);
 
+  const renderButtons = () => {
+    return (
+      <div className="flexDirectionRow  mb-3">
+        <button className="defaultArtistBtn">
+          <div
+            style={{
+              alignItems: "flex-start",
+            }}
+            className="flexDirectionCol"
+          >
+            {}
+            <span className="webfont -mb-1"></span>
+            <span className="artistName">Brää</span>
+          </div>
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div className="container">
       <div className="row my-3 my-sm-5">
@@ -12,21 +31,41 @@ export default function Home({ propsHere }) {
               <h1>Honey Pot Leaderboards</h1>
               <br></br>
               <div className="frame"></div>
-              <a href="/#/contract/0x9f3303e2c04e79387c3b5089b8a73e0b466e9076/sound">
-                Song 'Honey' on Optimism
-              </a>
+
               <br></br>
-              <a href="/#/contract/0xfcf069b5876ab35107e44906933cf67110a60bcd/sound">
-                Song 'Breathe' on Optimism
-              </a>
-              <br></br>
-              <a href="/#/contract/0x6a7463c342027ee8452f2686ccfa9b73026b4095/sound">
-                Song 'Break It - msft (VividFeverDreams Flip)' on Optimism
-              </a>
-              <br></br>
-              <a href="/#/contract/0xbd87f4da73ff92a7bea31e2de20e14f9829f42fe/zora">
-                Song 'The Keepers' - on Base
-              </a>
+
+              <div className="justify-center">
+                <div className="flexDirectionRow justify-center mb-3">
+                  <a
+                    className="btn btn-primary  homeBtnGroup"
+                    href="/#/contract/0x9f3303e2c04e79387c3b5089b8a73e0b466e9076/sound"
+                  >
+                    Song 'Honey' on Optimism
+                  </a>
+
+                  <a
+                    className="btn btn-primary  homeBtnGroup"
+                    href="/#/contract/0xfcf069b5876ab35107e44906933cf67110a60bcd/sound"
+                  >
+                    Song 'Breathe' on Optimism
+                  </a>
+
+                  <a
+                    className="btn btn-primary  homeBtnGroup"
+                    href="/#/contract/0x6a7463c342027ee8452f2686ccfa9b73026b4095/sound"
+                  >
+                    Song 'Break It - msft (VividFeverDreams Flip)' on Optimism
+                  </a>
+                </div>{" "}
+                <div className="flexDirectionRow justify-center mb-3">
+                  <a
+                    className="btn btn-primary  "
+                    href="/#/contract/0xbd87f4da73ff92a7bea31e2de20e14f9829f42fe/zora"
+                  >
+                    Song 'The Keepers' - on Base
+                  </a>
+                </div>
+              </div>
             </p>
           </div>
         </div>
