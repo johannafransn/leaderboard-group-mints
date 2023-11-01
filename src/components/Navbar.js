f98fcd2b4b4a6157ec2a998da7afbbad7b77bc33
Johanna Fransson <adrina0002@gmail.com>
2023-11-01 11:13:17 -0300

    change about text and url

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
                 d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
               />
             </svg>{" "}
-            Leaderboard{" "}
+            Honey Pot Leaderboards
           </a>
           <div className="navbar-collapse collapse show" id="navbarNav">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
--- src/pages/About.js
+++ src/pages/About.js
@@ -7,39 +7,47 @@ export default function About({ propsHere }) {
             <div className="card-body">
               <h5>Leaderboard</h5>
               <p>
-                This is a leaderboard for different mints. Rewards will be
-                distributed based on referrals and number of mints after mint is
-                over.
-              </p>
-              <p>
-                <a
-                  className="btn btn-primary "
-                  target="_blank"
-                  href="https://github.com/liquality/group-mint-contract"
-                  rel="noopener noreferrer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="17"
-                    height="17"
-                    fill="currentColor"
-                    className="bi bi-github"
-                    viewBox="0 0 16 16"
+                These are the Honey Pot Leaderboards. Created by Liquality,
+                Honey Pots allows creators to share with their supporters,
+                powered by smart measurable conditions, all onchain. As artists
+                flourish, so do their collectors. A true win-win. On November
+                14th, Honey Pots will be distributed for top contributors of
+                each drop based on their number of mints (with a 70% weight) and
+                number of referrals (with a 30% weight). <br></br>
+                <br></br>
+                <p>
+                  <a
+                    className="btn btn-primary  "
+                    target="_blank"
+                    href="https://x.com/Liquality_io/status/1719362747819872620?s=20"
+                    rel="noopener noreferrer"
                   >
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                  </svg>{" "}
-                  View code for rewards protocol
-                </a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="15"
+                      height="15"
+                      fill="currentColor"
+                      className="bi bi-twitter-x mb-1"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
+                    </svg>{" "}
+                    Read more here
+                  </a>
+                </p>
+                Top contributors for each drop will then participate in a raffle
+                to win the $150 Hive Bonus
               </p>
+
               <h5>Contact</h5>
               <p>
-                If you have any questions about this application or the rewards
-                mechanism, please reach out to us
-                <a href="mailto:support@liquality.io">
-                  {" "}
-                  support@liquality.io
+                If you have any questions about Honey Pots or would like to use
+                it to grow your reach onchain, please reach out to us{" "}
+                <a href="mailto:support@liquality.io">simon@liquality.io</a> or{" "}
+                <a target="_blank" href="https://t.me/simonlapscher">
+                  @simonlapscher
                 </a>{" "}
-                and we will get back to you as soon as possible.
+                on telegram.
               </p>
             </div>
           </div>
